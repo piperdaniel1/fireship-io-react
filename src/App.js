06fe876b088196dd58558114b7b41709ad3fa4b6
Daniel Piper <piperdaniel1@gmail.com>
2022-07-15 23:40:11 -0700

    #2 React Basics - 8:00

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import {createContext, useContext, useEffect, useRef, useState} from 'react'
+import './App.css'
+
+const moods = {
+  happy: 'happy',
+  sad: 'not sad',
+  angry: 'angry'
+}
+
+const MoodContext = createContext(moods)
 
 function App() {
+  // create a count variable in the state with a default value of zero.
+  const [count, setCount] = useState(0)
+
+  const myBtn = useRef(null)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-title">Welcome to React</h1>
+
+      <p> The current count is {count} </p>
+
+      <MoodContext.Provider value={moods.sad}>
+        <MoodBox />
+      </MoodContext.Provider>
+
+      <button ref={myBtn} onClick={() => {
+          setCount(count+2)
+          moods.sad = 'not sad at all'
+        }
+      }>
+        Increase Count!
+      </button>
+
+      <button onClick={() => myBtn.current.click()}>This clicks the other button</button>
     </div>
-  );
+  )
+}
+
+function MoodBox() {
+  const mood = useContext(MoodContext)
+  
+  return (
+    <p> The mood is currently {mood}.</p>
+  )
 }
 
 export default App;
